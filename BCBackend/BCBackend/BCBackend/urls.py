381daa769fbb2a39bee81b3e617b90c77958eda3
Jabril Hall <jabril6187@gmail.com>
2024-04-06 20:55:39 -0400

    Started on django view created a dummy data set`

--- BCBackend/BCBackend/BCBackend/urls.py
+++ BCBackend/BCBackend/BCBackend/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from userData2.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('timeline', TimelineView.as_view(), name="TimelineView")
 ]
--- BCBackend/BCBackend/userData2/views.py
+++ BCBackend/BCBackend/userData2/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
+from django.conf import settings
 
+from .models import *
+from rest_framework.response import Response
+
+
+class UserFormView(APIView):
+    def post(self, request):
+        pass
+
+class TimelineView(APIView):
+    def get(self, request):
+        output = {
+            'jobs':[{
+                'job': "Software Eng",
+                "skills":["python", "html", "react", "javascript"]
+            },
+                {
+                    'job': "Flouriest",
+                    'skills':['cutting', 'watering', "growing"]
+                }]
+        }
+        return Response(output)
 # Create your views here.
